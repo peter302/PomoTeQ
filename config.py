895aab3d1405835212b64777de0fbcfaed302152
peter kuria <you@example.com>
2020-01-17 03:21:19 -0500

    initialized app extension

---
+++ config.py
@@ -0,0 +1,29 @@
+import os
+
+class Config:
+    """Main configurations class"""
+
+
+
+    pass
+
+
+
+
+
+class ProdConfig(Config):
+    """Production configuration class that inherits from the main configurations class"""
+
+    pass
+
+
+class DevConfig(Config):
+    """Configuration class for development stage of the app"""
+    DEBUG = True
+
+
+config_options = {
+    'development': DevConfig,
+    'production': ProdConfig
+    #'test':TestConfig
+}
